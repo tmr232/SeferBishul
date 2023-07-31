33c8b5dada765a23bb0d70870e88c6f2ac389248
Tamir Bahar <tamir.bahar@gmail.com>
2023-07-31 17:50:42 +0300

    Support for nesting!
    Just need to wire things, so committing before ;)

--- src/sefer_bishul/main.py
+++ src/sefer_bishul/main.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import enum
 import importlib.resources
+import itertools
 import operator
 import os
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Callable
 
 import typer
 import more_itertools
@@ -52,16 +54,73 @@ class Heading:
 Line = str | Heading
 
 
+def render_toplevel(section: Section) -> str:
+    env = get_env()
+    template = env.get_template("toplevel.partial.html")
+    return template.render(
+        heading=section.heading,
+        # Remove empty lines
+        description="\n".join(filter(bool, section.content)),
+    )
+
+
+def render_ingredients(section: Section) -> str:
+    env = get_env()
+    template = env.get_template("ingredients.partial.html")
+    return template.render(
+        heading=section.heading,
+        # Remove empty lines
+        ingredients=list(filter(bool, section.content)),
+    )
+
+
+def render_prep(section: Section) -> str:
+    env = get_env()
+    template = env.get_template("prep.partial.html")
+    return template.render(
+        heading=section.heading,
+        # Merge blocks
+        steps=[
+            "\n".join(lines)
+            for lines in more_itertools.split_at(section.content, lambda s: not s)
+        ],
+    )
+
+
+_RENDER_MAPPING = {
+    render_ingredients: ("חומרים",),
+    render_prep: (
+        "הוראות",
+        "הוראות הכנה",
+    ),
+}
+
+_HEADING_TO_RENDER_MAPPING = {
+    value: key for key, values in _RENDER_MAPPING.items() for value in values
+}
+
+
+def heading_to_renderer(heading: Heading) -> Callable[[Section], str] | None:
+    if heading.level != 2:
+        # Only the second level of heading is interesting here.
+        return None
+    return _HEADING_TO_RENDER_MAPPING.get(heading)
+
+
 @attrs.frozen
 class Section:
     heading: Heading
     content: list[str]
+    children: list[Section] = attrs.field(factory=list)
 
     @staticmethod
     def from_lines(lines: Iterable[Line]) -> Section:
         heading, *content = lines
         assert isinstance(heading, Heading)
-        return Section(heading=heading, content=content)
+        return Section(
+            heading=heading,
+            content=content,
+        )
 
 
 def parse_heading(line: str) -> Heading | None:
@@ -76,6 +135,70 @@ def parse_line(line: str) -> str | Heading:
     if heading := parse_heading(line):
         return heading
     return line
+
+
+def parse_sections(recipe: str) -> list[Section]:
+    parsed_lines = map(parse_line, recipe.splitlines())
+
+    section_lines = more_itertools.split_before(
+        parsed_lines, lambda s: isinstance(s, Heading)
+    )
+
+    return list(map(Section.from_lines, section_lines))
+
+
+def nest_sections(sections: list[Section]) -> Section:
+    # Assume sections is non-empty
+    assert sections
+    # Assume the toplevel exists, and it's the only one
+    assert sections[0].heading.level == 1
+
+    # We use copy to avoid modifying the input sections.
+    def copy(section: Section) -> Section:
+        return attrs.evolve(section)
+
+    toplevel = copy(sections[0])
+
+    stack = [toplevel]
+    for section in sections[1:]:
+        while stack and section.heading.level <= stack[-1].heading.level:
+            stack.pop()
+        stack[-1].children.append(copy(section))
+        stack.append(section)
+
+    return toplevel
+
+
+def render_nested_sections(toplevel: Section) -> str:
+    """
+    The idea is that as we traverse the section tree,
+    special sections (with their name & level in the heading mapping)
+    get relevant CSS classes added to their elements, for formatting!
+
+    And possible additional rendering features.
+    """
+    rendered_sections = []
+
+    stack = [(toplevel, render_toplevel)]
+    while stack:
+        section, renderer = stack.pop()
+        renderer = heading_to_renderer(section.heading) or renderer
+
+        # TODO: Maybe extract to generator?
+        rendered_sections.append(renderer(section))
+
+        for child in section.children:
+            stack.append((child, renderer))
+
+    return "\n".join(rendered_sections)
+
+
+def render_recipe_from_nested(toplevel: Section) -> str:
+    recipe = render_nested_sections(toplevel)
+
+    env = get_env()
+    template = env.get_template("recipe.container.html")
+    return template.render(recipe=recipe)
 
 
 def parse_recipe_text(recipe: str) -> Recipe:
