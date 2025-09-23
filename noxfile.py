6c3713bde7570ec8d1c2a9a7db4d33c83d045e0c
Tamir Bahar <tamir.bahar@gmail.com>
2025-09-23 22:44:39 +0300

    Migrate to uv

--- noxfile.py
+++ noxfile.py
@@ -9,16 +9,13 @@ nox.options.sessions = ["format", "lint", "test"]
 @nox.session(python=False)
 def format(session: Session) -> None:
     session.run("ruff", "check", "--fix-only", "--exit-zero", *SOURCE_LOCATIONS)
-    session.run("isort", *SOURCE_LOCATIONS)
-    session.run("black", *SOURCE_LOCATIONS)
+    session.run("ruff", "format", *SOURCE_LOCATIONS)
 
 
 @nox.session(python=False)
 def lint(session: Session) -> None:
-    session.run("ruff", *SOURCE_LOCATIONS)
+    session.run("ruff", "check", *SOURCE_LOCATIONS)
     session.run("mypy", *SOURCE_LOCATIONS)
-    session.run("isort", "--check-only", *SOURCE_LOCATIONS)
-    session.run("black", "--check", *SOURCE_LOCATIONS)
 
 
 @nox.session(python=False)
